67157797cd285e7b4d572cca914cd6b6907d2bc1
Duncan Parkes <duncan@deparkes.co.uk>
2014-11-10 18:38:15 +0000

    Sort out while True:, make presentable.

--- FileLoadLoop/FileLoadLoop.py
+++ FileLoadLoop/FileLoadLoop.py
@@ -1,12 +1,12 @@
+# FileLoadLoop.py
+# Part of the PythonTips series on deparkes.co.uk
+# Duncan Parkes
+
 # Load our initial list of files
 FileList = open('./FileList.txt').read().splitlines()
 
 # Let's also display it to make sure we have loaded the right list
 print FileList
-
-# FileLoadLoop.py
-# Part of the PythonTips series on deparkes.co.ukqa
-# Duncan Parkes
 
 # We will loop through the different items in this newly load list.
 # After each iteration we will check to see if the list has been updated, and
@@ -16,21 +16,21 @@ FileList_length = len(FileList)
 while file_count < FileList_length:
 	print "Running script on %s" % (FileList[file_count])
 	print "Modify the file now, if you want"
+	# raw_input() as a way to wait for the user to be ready to move on
 	a=raw_input()
 	
-
+	# Use a try - except block to catch any failure to load the file.
 	try:
 		file_list_new = open('./FileList.txt').read().splitlines()
 		if FileList != file_list_new:
 			FileList = file_list_new
 			print(FileList)
 			print('New list loaded successfully');
-		break
-	except ValueError:
-		print('Error loading new file\nWill try again next time.')
+	except IOError:
+		print('Error loading new file\nWill continue to use existing list and try again next time.')
+		
 	FileList_length = len(FileList)
 	file_count = file_count + 1
-       
         
 print "Press return to continue"
 a=raw_input()
